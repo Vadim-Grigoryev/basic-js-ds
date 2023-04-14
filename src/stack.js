7f5e1f89b8c6d8cabc1f32b02092fc8b8dc4c1de
Vadim-Grigoryev <minisefo@mail.ru>
2023-04-14 17:42:40 +0300

    Just stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (data) {
+    this.data=[]
+  }
+  push(element) {
+    this.data.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let a
+    return a=this.data.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let b=this.data.slice(-1).join('')
+    return +b
   }
 }
 
